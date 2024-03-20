ca9f0064ac34690e22a9bdbaa26510c3fcbfd73d
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-03-20 19:40:06 +0100

    mini bank exercise from zajavka 8 workshop

--- src/main/java/pl.zajavka/BankService.java
+++ src/main/java/pl.zajavka/BankService.java
@@ -72,6 +72,53 @@ public class BankService {
     }
 
 
+    public void transfer(long fromAccountId, long toAccountId, BigDecimal amount) throws BankException {
+        log.info("Making transfer {} from account with id {} to account with id {}", amount, fromAccountId, toAccountId);
+        var optionalFromAccount = findAccountById(fromAccountId);
+        var optionalToAccount = findAccountById(toAccountId);
 
+        if (optionalFromAccount.isEmpty()) {
+            log.error("Account with id {} does not exist", fromAccountId);
+            throw new BankException("Account with id %s does not exist".formatted(fromAccountId));
+        }
+
+        if (optionalToAccount.isEmpty()) {
+            log.error("Account with id {} does not exist", optionalToAccount);
+            throw new BankException("Account with id %s does not exist".formatted(optionalToAccount));
+        }
+
+        var fromAccount = optionalFromAccount.get();
+        var toAccount = optionalToAccount.get();
+
+        if (fromAccount.getBalance().compareTo(amount) < 0) {
+            log.error("Account {} has an insufficient balance", fromAccountId);
+            throw new BankException("Account %s has an insufficient balance".formatted(fromAccountId));
+        }
+
+        fromAccount.setBalance(fromAccount.getBalance().subtract(amount));
+        toAccount.setBalance(toAccount.getBalance().add(amount));
+        log.info("Transfer of {} made from account with id {} to account with id {}", amount, fromAccountId, toAccountId);
+
+        long id = ++lastTransactionId;
+        var fromTransaction = BankTransaction.of(id, fromAccount, amount.negate(), LocalDateTime.now());
+        var toTransaction = BankTransaction.of(id, toAccount, amount, LocalDateTime.now());
+        transactions.add(fromTransaction);
+        transactions.add(toTransaction);
+        log.debug("Registered transaction {} from account with id {} to account with id {}", amount, fromAccountId, toAccountId);
+
+    }
+
+    public List<BankTransaction> getTransactionsHistory(long accountId) throws BankException {
+        log.info("Getting transations history for account {}", accountId);
+        var optionalAccount = findAccountById(accountId);
+        if (optionalAccount.isEmpty()) {
+            log.error("Account with id {} does not exist", accountId);
+            throw new BankException("Account with id %s does not exist".formatted(accountId));
+        }
+
+        return transactions.stream()
+                .filter(transaction -> transaction.getAccount().getId() == accountId)
+                .toList();
+    }
 
 }
