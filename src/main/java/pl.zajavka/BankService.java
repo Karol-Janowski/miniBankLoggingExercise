84b845c3c429ad8b243eb293b990c0d01b113dea
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-03-20 19:05:47 +0100

    mini bank exercise from zajavka 8 workshop

--- src/main/java/pl.zajavka/BankService.java
+++ src/main/java/pl.zajavka/BankService.java
@@ -33,8 +33,8 @@ public class BankService {
                 .findFirst();
     }
 
-    public void deposit(long accountId, BigDecimal ammount) throws BankException {
-        log.info("Making a deposit {} on an account with id {}", ammount, accountId);
+    public void deposit(long accountId, BigDecimal amount) throws BankException {
+        log.info("Making a deposit {} on an account with id {}", amount, accountId);
         var optionalAccount = findAccountById(accountId);
         if (optionalAccount.isEmpty()) {
             log.error("Account with id {} does not exist.", accountId);
@@ -42,12 +42,33 @@ public class BankService {
         }
 
         var account = optionalAccount.get();
-        account.setBalance(account.getBalance().add(ammount));
-        log.info("Deposit {} make on account with id {}.", ammount, accountId);
+        account.setBalance(account.getBalance().add(amount));
+        log.info("Deposit {} make on account with id {}.", amount, accountId);
         long id = ++lastTransactionId;
-        var transaction = BankTransaction.of(id, account, ammount, LocalDateTime.now());
+        var transaction = BankTransaction.of(id, account, amount, LocalDateTime.now());
         transactions.add(transaction);
         log.debug("Registered transaction {} for an accoutn with id {}...", id, accountId);
+    }
+
+    public void withdraw(long accountId, BigDecimal amount) throws BankException {
+        log.info("Making a withdrawal {} from account with id {}", amount, accountId);
+        var optionalAccount = findAccountById(accountId);
+        if (optionalAccount.isEmpty()) {
+            log.error("Account with id {} does not exist", accountId);
+            throw new BankException("Account with id %s does not exist".formatted(accountId));
+        }
+
+        var account = optionalAccount.get();
+        if (account.getBalance().compareTo(amount) < 0) {
+            log.error("Account {} has an insufficient balance", accountId);
+            throw new BankException("Account %s has an insufficient balance".formatted(accountId));
+        }
+        account.setBalance(account.getBalance().subtract(amount));
+        log.info("Withdrawal {} was made from account with id {}", amount, account);
+        long id = ++lastTransactionId;
+        var transaction = BankTransaction.of(id, account, amount.negate(), LocalDateTime.now());
+        transactions.add(transaction);
+        log.debug("Registered transaction {} for account with id {}", id, accountId);
     }
 
 
